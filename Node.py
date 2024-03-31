522dc9fd2a16da42fb9a2a8860ff441470c51bdb
“Max <mparis@hotmail.cl>
2024-03-30 23:19:55 -0600

    Node created

---
+++ Node.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self):
+        self.reducedMatrix = None
+        self.lowerBound = None
+        #  self.currentCityNumber = None
+        self.level = None
+        self.pathVisited = []
+
